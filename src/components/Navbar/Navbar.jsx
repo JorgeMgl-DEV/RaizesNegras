e6b45df326f1ae81f76c9233fe44b04dd62cec21
JorgeMgl-DEV <jorgeditor.social@gmail.com>
2025-07-18 20:05:00 -0300

    com mapa funcianando sem fill

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import './Navbar.css';
+import logoRclaro from '../../assets/logoRclaro.png';
+
+function Navbar(){
+    return(
+        <>
+        <nav className="navbar">
+        <div className="navbar__logo">
+            <a href="#"><img src={logoRclaro} alt="Logo Raízes Negras" /></a>
+        </div>
+        <div className="navbar__direita">
+            <div className="navbar__menu">
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Conteúdo</a></li>
+                    <li><a href="#">FAQ & Contato</a></li>
+                </ul>
+            </div>
+            <div className="navbar__search">
+                <input type="text" placeholder="Buscar..."></input>
+                <button className="fa fa-search" aria-label="Buscar"></button>
+            </div>
+        </div>
+    </nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar/Navbar"
+import InteractiveCircle from '../components/Navbar/Circle';
+function Home(){
+    return(
+        <>
+        <Navbar></Navbar>
+        <InteractiveCircle />
+        </>
+    )
+}
+
+export default Home
