d765556917f3d5a51f765d9f48670d353cfba519
JorgeMgl-DEV <jorgeditor.social@gmail.com>
2025-08-08 12:52:56 -0300

    topsectcomplete

--- src/components/top-section/TopSection.jsx
+++ src/components/top-section/TopSection.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import MapaMA from './Mapa/Mapa-MA';
+import logoRdark from '../../assets/logoRdark.png';
 import './TopSection.css';
 
 const TopSection = () => {
     return (
         <div className="top-section">
             <Navbar />
-            <MapaMA />
+            <div className="content-section">
+                <div className="project-info">
+                    <img src={logoRdark} alt="Logo Raízes Negras" className="project-logo" />
+                    <div className="project-description"> 
+                        <p>Raízes Negras é um acervo cultural dedicado à preservação da herança afrodescendente no Maranhão.</p>
+                    </div>
+                </div>
+                <MapaMA />
+            </div>
             <div className="scroll-arrow">
                 ↓
             </div>
