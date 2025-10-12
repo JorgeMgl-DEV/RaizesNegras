a7b22ca296d11943ff24dd4f603ba3d211db3578
Jorge Miguel Viana Torrees <jorgeditor.social@gmail.com>
2025-10-12 15:40:49 +0000

    analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/HOME/Home.jsx"
-import RegionPage from "./pages/REGIAO/RegionPage.jsx"
-import Sobre from "./pages/SOBRE/Sobre.jsx"
-import Conteudo from "./pages/CONTEUDO/Conteudo.jsx"
-import Faq from "./pages/FAQ/Faq.jsx"
-import Article from "./pages/ARTIGO/Article.jsx"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/HOME/Home.jsx";
+import RegionPage from "./pages/REGIAO/RegionPage.jsx";
+import Sobre from "./pages/SOBRE/Sobre.jsx";
+import Conteudo from "./pages/CONTEUDO/Conteudo.jsx";
+import Faq from "./pages/FAQ/Faq.jsx";
+import Article from "./pages/ARTIGO/Article.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/regiao/:slug" element={<RegionPage />} />
-      <Route path="/sobre" element={<Sobre />} />
-      <Route path="/conteudo" element={<Conteudo />} />
-      <Route path="/artigo/:id" element={<Article />} />
-      <Route path="/faq" element={<Faq />} />
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/regiao/:slug" element={<RegionPage />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/conteudo" element={<Conteudo />} />
+        <Route path="/artigo/:id" element={<Article />} />
+        <Route path="/faq" element={<Faq />} />
+      </Routes>
+
+      <Analytics />
+    </>
+  );
 }
