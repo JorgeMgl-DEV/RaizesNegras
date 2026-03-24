0593f196137e5ce40f673c5114c33c18de0a316a
Jorge Miguel Viana Torrees <jorgeditor.social@gmail.com>
2026-03-24 20:34:19 -0300

    Revert "feat: add Firebase authentication setup"

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { AuthProvider } from './context/AuthContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
       <App />
-    </AuthProvider>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
 );
